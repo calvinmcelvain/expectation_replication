1c30e7f9fb9a949758eaa888937a964b3795de35
calvinmcelvain <133781550+calvinmcelvain@users.noreply.github.com>
2024-03-24 20:22:58 -0500

    03/24/2024: Revised Cleaning

--- code/_001cleaning.py
+++ code/_001cleaning.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 ###############################################
             ## GENERAL CLEANING ##
@@ -12,62 +11,35 @@ import numpy as np
 ### Mean Forecasts ###
 mean_spf = pd.read_csv("Documents/thesis/_replication/data/spf_mean_cpi.csv")
 mean_spf.to_csv("Documents/thesis/_replication/data/spf_mean_cpi.csv", sep=',', index=False)  # Original file had CRLF endings, changed to LF for Git
-
 mean_spf_trim = mean_spf
-mean_spf_trim['yrq'] = mean_spf_trim['YEAR'].astype(str) + '_Q' + mean_spf_trim['QUARTER'].astype(str)
-mean_spf_trim = mean_spf_trim.fillna("")
-mean_spf_trim = mean_spf_trim.drop(['CPIA', 'CPIB', 'CPIC'], axis=1)
-mean_spf_trim = mean_spf_trim.rename(columns={'CPI1':'f_t0', 'CPI2': 'f_t1', 'CPI3': 'f_t2', 'CPI4': 'f_t3', 'CPI5': 'f_t4', 'CPI6': 'f_t5'})
 
-def filter_date(dataframe, a, b):
-    if a != 0:
-        c = dataframe[(dataframe['yrq'] >= a)]
-        if b != 0:
-            d = c[(c['yrq'] <= b)]
-            return d
-        else:
-            return c
-    else:
-        if b != 0:
-            d = c[(c['yrq'] <= b)]
-            return d
-        else:
-            return c
+mean_spf_trim = mean_spf_trim.set_index(pd.to_datetime(mean_spf_trim['YEAR'].astype(str) + '-' + (mean_spf_trim['QUARTER'] * 3).astype(str), format='%Y-%m') + pd.offsets.MonthEnd(0))
 
-mean_spf_trim = filter_date(mean_spf_trim, '1981_Q3', 0)
-mean_spf_trim = mean_spf_trim.reset_index()
+mean_spf_trim = mean_spf_trim.drop(['YEAR', 'QUARTER', 'CPIA', 'CPIB', 'CPIC'], axis=1)
+mean_spf_trim = mean_spf_trim['1981-09-01':]
 
 ### Individual Forecasts ###
 ind_spf = pd.read_csv("Documents/thesis/_replication/data/spf_ind_cpi.csv")
 ind_spf.to_csv("Documents/thesis/_replication/data/spf_ind_cpi.csv", sep=',', index=False)  # Original file had CRLF endings, changed to LF for Git
-
 ind_spf_trim = ind_spf
-ind_spf_trim['yrq'] = ind_spf_trim['YEAR'].astype(str) + '_Q' + ind_spf_trim['QUARTER'].astype(str)
-ind_spf_trim = ind_spf_trim.drop(['CPIA', 'CPIB', 'CPIC'], axis=1)
-ind_spf_trim = ind_spf_trim.rename(columns={'CPI1':'f_t0', 'CPI2': 'f_t1', 'CPI3': 'f_t2', 'CPI4': 'f_t3', 'CPI5': 'f_t4', 'CPI6': 'f_t5'})
 
+ind_spf_trim = ind_spf_trim.set_index([pd.to_datetime(ind_spf_trim['YEAR'].astype(str) + '-' + (ind_spf_trim['QUARTER'] * 3).astype(str), format='%Y-%m') + pd.offsets.MonthEnd(0), 'ID'])  # Panel data
 
-ind_spf_trim = filter_date(ind_spf_trim,'1981_Q3', 0)
-ind_spf_trim = ind_spf_trim.reset_index()
+ind_spf_trim = ind_spf_trim.drop(['YEAR', 'QUARTER', 'CPIA', 'CPIB', 'CPIC'], axis=1)
+ind_spf_trim = ind_spf_trim['1981-09-01':]
 
 #####################
-  ## VINTAGE DATA ##
+ ## VINTAGE DATA ##
 #####################
+
 vintage = pd.read_csv("Documents/thesis/_replication/data/vintage_cpi.csv")
 vintage.to_csv("Documents/thesis/_replication/data/vintage_cpi.csv", sep=',', index=False)    # Original file had CRLF endings, changed to LF for Git
-
 vintage_trim = vintage
+
 vintage_trim['DATE'] = pd.to_datetime(vintage_trim['DATE'], format='%Y:%m')
 vintage_trim.set_index('DATE', inplace=True)
 vintage_trim = vintage_trim.resample('Q').mean()    # Data is collected monthly, mean aggregating to quarterly to match forecasts
-vintage_trim.reset_index(inplace=True)
-vintage_trim['yrq'] = vintage_trim['DATE'].dt.strftime('%Y_Q') + vintage_trim['DATE'].dt.quarter.astype(str)
-vintage_trim['year'] = vintage_trim['yrq'].str.split('_').str[0].astype(int)
-vintage_trim['quarter'] = vintage_trim['yrq'].str.split('Q').str[1].astype(int)
-vintage_trim.drop(columns=['DATE'], inplace=True)
-
-vintage_trim = filter_date(vintage_trim, '1965_Q1', 0)
-vintage_trim = vintage_trim.reset_index()
+vintage_trim = vintage_trim['1965-03-01':]
 
 def drop_cols(dfcols, a, b, di):
     columns = []
@@ -84,65 +56,78 @@ def drop_cols(dfcols, a, b, di):
 vintage_trim = vintage_trim.drop(columns=drop_cols(vintage_trim.columns, 64, 95, ['CPI94Q3', 'CPI94Q4']))
 
 ###############################################
-              ## CALCULATIONS ##
+        ## PRELIMINARY CALCULATIONS ##
 ###############################################
 
-### Vintage Growth ###
-def cpi_growth(dataframe):
-    for t in range(1, len(dataframe)-5):
-        y = dataframe.loc[t - 1, 'year']
-        q = dataframe.loc[t - 1, 'quarter']
-        
-        if q <= 3:
-            if y <= 1994 and pd.notna(dataframe.loc[t - 1, 'CPI94Q3']):
-                dataframe.loc[t, 't'] = dataframe.loc[t - 1, 'CPI94Q3']
-            else:
-                y = dataframe.loc[t, 'year']
-                q = dataframe.loc[t, 'quarter']
-                dataframe.loc[t, 't'] = dataframe.loc[t - 1, f'CPI{str(y)[2:4]}Q{q}']
-        else:
-            if y <= 1994 and pd.notna(dataframe.loc[t - 1, 'CPI94Q3']):
-                dataframe.loc[t, 't'] = dataframe.loc[t - 1, 'CPI94Q3']
-            else:
-                y = dataframe.loc[t, 'year']
-                q = dataframe.loc[t, 'quarter']
-                dataframe.loc[t, 't'] = dataframe.loc[t - 1, f'CPI{str(y)[2:4]}Q{q}']
+#####################
+ ## ACTUAL GROWTH ##
+#####################
+
+### Actuals ###
+def actual_growth(df):
+    for i in vintage_trim.index[1:]:
+        column_name = ('CPI' + str(i.year)[-2:] + 'Q' + str((i.month - 1) // 3 + 1)) if i > pd.Timestamp('1994-09-30') else 'CPI94Q3'
+        df.loc[i, 't'] = vintage_trim.loc[i - pd.offsets.QuarterEnd(), column_name]
+    
+    for i in df.index[1:]:
+        for j in range(1, 6):
+            next_quarters = i + pd.offsets.QuarterEnd(j)
+            if next_quarters in df.index:
+                df.loc[i, f't{j}'] = ((df.loc[next_quarters, 't'] / df.loc[i, 't']) - 1)
+actual_growth(vintage_trim)
+
+### Forecasts ###
+def cpi_growth(df):
+    for i in df.index:
+        df.loc[i, 'f_t0'] = df.loc[i, 'CPI2']/400
+        for j in range(1,5):
+            df.loc[i, f'f_t{j}'] = (df.loc[i, f'f_t{j-1}']+1) * (df.loc[i, f'CPI{j+2}']/400 +1) -1
             
-        for i in range(0, 5):
-            y_i = dataframe.loc[t + i + 1, 'year']
-            q_i = dataframe.loc[t + i + 1, 'quarter']
-            if q_i <= 3:
-                if y_i <= 1994 and pd.notna(dataframe.loc[t + i, 'CPI94Q3']):
-                    dataframe.loc[t, f't{i}'] = ((dataframe.loc[t + i, 'CPI94Q3'] / dataframe.loc[t, 't']) - 1) * 100
-                else:
-                    dataframe.loc[t, f't{i}'] = ((dataframe.loc[t + i, f'CPI{str(y_i)[2:4]}Q{str(q_i)}'] / dataframe.loc[t, 't']) - 1) * 100
-            else:
-                if y_i <= 1994 and pd.notna(dataframe.loc[t + i, 'CPI94Q3']):
-                    dataframe.loc[t, f't{i}'] = ((dataframe.loc[t + i, 'CPI94Q3'] / dataframe.loc[t, 't']) - 1) * 100
-                else:
-                    dataframe.loc[t, f't{i}'] = ((dataframe.loc[t + i, f'CPI{str(y_i)[2:4]}Q{str(q_i)}'] / dataframe.loc[t, 't']) - 1) * 100
+cpi_growth(mean_spf_trim)
+cpi_growth(ind_spf_trim)
 
-cpi_growth(vintage_trim)
+#####################
+   ## Revisions ##
+#####################
 
-### Revisions ###
-
-# Mean SPF data
-def revisions(dataframe):
-    for n in range(0, len(dataframe)-2):
-        for i in range(1, 5):
-            dataframe.loc[n, f'r_t{i}'] = dataframe.loc[n + 1, f'f_t{i}'] - dataframe.loc[n, f'f_t{i + 1}']
+### Mean Forecasts ###
+def revisions(df):
+    for i in df.index:
+        for j in range(0, 4):
+            if (i - pd.offsets.QuarterEnd()) in df.index:
+                df.loc[i, f'r_t{j}'] = df.loc[i, f'f_t{j}'] - ((df.loc[i - pd.offsets.QuarterEnd(), f'f_t{j + 1}'] + 1) / (df.loc[i - pd.offsets.QuarterEnd(), 'f_t0'] + 1) - 1)    # Only revisions since t-1
 
 revisions(mean_spf_trim)
 
-# Individual SPF data
-def revisions_ind(dataframe):
-    a = np.unique(dataframe['ID'])
-    for t in a:
-        b = dataframe[dataframe['ID'] == t]
-        for k in b:
-            for i in range(1, 5):
+### Individual Forecasts ###
+def revisions_ind(df):
+    for d, i in df.index:
+        for j in range(0, 4):
+            if (d- pd.offsets.QuarterEnd(), i) in df.index:
+                previous_quarter = d - pd.offsets.QuarterEnd()
+                df.loc[(d, i), f'r_t{j}'] = df.loc[(d, i), f'f_t{j}'] - ((df.loc[(previous_quarter, i), f'f_t{j + 1}'] + 1) / (df.loc[(previous_quarter, i), 'f_t0'] + 1) - 1)    # Only revisions since t-1
 
-o = revisions_ind(ind_spf_trim)
+revisions_ind(ind_spf_trim)
+
+#####################
+    ## Errors ##
+#####################
+
+### Mean Forecasts ###
+def errors(df, v):
+    for i in df.index:
+        for j in range(1, 6):
+            df.loc[i, f'e_t{j}'] = v.loc[i, f't{j}'] - df.loc[i, f'f_t{j-1}']
+
+errors(mean_spf_trim, vintage_trim)
+
+### Individual Forecasts ###
+def errors_ind(df, v):
+    for d, i in df.index:
+        for j in range(1, 6):
+            df.loc[(d, i), f'e_t{j}'] = v.loc[d, f't{j}'] - df.loc[(d, i), f'f_t{j-1}']
+
+errors_ind(ind_spf_trim, vintage_trim)
 
 ###############################################
                 ## EXPORT ##
--- code/_002sumstats.py
+++ code/_002sumstats.py
@@ -1,56 +1,18 @@
 import pandas as pd
-import numpy as np
-from _001cleaning import filter_date
+
+mean_spf = pd.read_csv('Documents/thesis/_replication/cleaned_data/mean_spf_trim.cvs')
+ind_spf = pd.read_csv('Documents/thesis/_replication/cleaned_data/ind_spf_trim.cvs')
+
+mean_spf = mean_spf.set_index('Unnamed: 0')
+ind_spf = ind_spf.set_index(['Unnamed: 0', 'ID'])
+
 
 ###############################################
-                ## REVISIONS ##
+                ## ERRORS ##
 ###############################################
-
-#####################
-    ## MEAN SPF ##
-#####################
-
-mean_spf_trim = pd.read_csv('Documents/thesis/_replication/cleaned_data/mean_spf_trim.cvs')
-vintage_trim = pd.read_csv('Documents/thesis/_replication/cleaned_data/vintage_trim.cvs')
-
-working_mean_data = pd.merge(mean_spf_trim, vintage_trim[['t', 't0', 't1', 't1', 't2', 't3', 't4']], on='yrq', how='right')
-s = pd.read_stata('Documents/thesis/models/cg/Replication_files/workfiles/us_data_updated.dta')
-l = pd.read_stata('Documents/thesis/models/cg/Replication_files/Data/Macro-data---FRED.dta')
-j = pd.read_stata('Documents/thesis/models/diagnostic/programs/data/spf/iCPI.dta')
-
-# Calculating mean and std of revisions (all horizons)
-def mean_revisions(dataframe):
-    list = []
-    for i in range(1, 5):
-        list.append(np.mean(dataframe[f'r_t{i}']))
-    a = np.array(list)
-    b = np.mean(a)
-    return a, b
-
-def std_revisions(dataframe):
-    list = []
-    stacked_df = pd.concat([dataframe[f'r_t{k}'] for k in range(1, 5)], axis=1).stack()
-    for i in range(1, 5):
-        list.append(np.std(dataframe[f'r_t{i}']))
-    a = np.array(list)
-    b = np.std(stacked_df)
-    return a, b
-
-revision_means, revision_mean = mean_revisions(working_mean_data)
-revision_stds, revision_std = std_revisions(working_mean_data)
-
-#####################
-## INDIVIDUAL SPF ##
-#####################
-
-ind_spf_trim = pd.read_csv('Documents/thesis/_replication/cleaned_data/ind_spf_trim.cvs')
-working_ind_data = pd.merge(ind_spf_trim, vintage_trim[['t', 't0', 't1', 't1', 't2', 't3', 't4']], on='yrq', how='right')
-
-# Filter date
-working_ind_data = filter_date(working_ind_data, '1981Q3', 0)
 
 
 
 ###############################################
-                ## Errors ##
+            ## REVISIONS ##
 ###############################################
