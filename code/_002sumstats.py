510f87eaf03f97ea11e704f818dd14d042b52486
calvinmcelvain <mcelvain1@hotmail.com>
2024-03-25 13:57:07 -0500

    03/25/2024: Estimation 1.0.0

--- code/_002sumstats.py
+++ code/_002sumstats.py
@@ -16,14 +16,33 @@ ind_stats = pd.DataFrame()
 mean_spf = mean_spf[:'2016-12-31']  # Filter data
 
 
-def revision_stats(df, v):
+def sum_stats(df, v):
     for j in range(4):
         v.loc[j, 'mean_revisions'] = np.mean(df[f'r_t{j}'] * 100)
         v.loc[j, 'std_revisions'] = np.std(df[f'r_t{j}'] * 100)
+        upward = 0
+        downward = 0
+        na = 0
+        total = 0
+        for i in df.index:
+            if pd.notna(df.loc[i, f'r_t{j}']):
+                if df.loc[i, f'r_t{j}'] > 0:
+                    upward += 1
+                    total += 1
+                elif df.loc[i, f'r_t{j}'] < 0:
+                    downward += 1
+                    total += 1
+                else:
+                    na += 1
+                    total += 1
+                v.loc[j, 'revisions_up'] = upward/total
+                v.loc[j, 'revisions_downn'] = downward/total
+                v.loc[j, 'revisions_na'] = na/total
     
     for j in range(5):
         v.loc[j, 'mean_errors'] = np.mean(df[f'e_t{j+1}'] * 100)
         v.loc[j, 'std_errors'] = np.std(df[f'e_t{j+1}'] * 100)
 
-revision_stats(mean_spf, mean_stats)
-revision_stats(ind_spf, ind_stats)
+sum_stats(mean_spf, mean_stats)
+sum_stats(ind_spf, ind_stats)
+
--- code/_003estimation.py
+++ code/_003estimation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import statsmodels.api as sm
 import numpy as np
 
 mean_spf = pd.read_csv('Documents/thesis/_replication/cleaned_data/mean_spf_trim.csv')
@@ -10,3 +11,12 @@ ind_spf = ind_spf.set_index(['Unnamed: 0', 'ID'])
 ###############################################
                 ## ESTIMATION ##
 ###############################################
+
+### Mean Forecasts ###
+revisions3 = mean_spf[1:]['r_t3']
+errors3 = mean_spf[1:]['e_t3']
+
+initial = sm.OLS(errors3, revisions3).fit()
+reg = initial.get_robustcov_results(cov_type='HAC',maxlags=1)
+
+print(reg.summary())
